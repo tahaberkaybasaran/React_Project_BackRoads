5192ac48ae5377715e3d53a3316808f61cccdf5a
Trigonometri1 <tahaberkaybasaran@gmail.com>
2023-09-03 00:50:07 +0300

    service component refactor

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Title from "./Title";
+import { services } from "../data/links";
 
 const Services = () => {
   return (
@@ -7,44 +8,20 @@ const Services = () => {
       <Title className="section-title" text1="our" text2="services" />
 
       <div className="section-center services-center">
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-wallet fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">saving money</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
-
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-tree fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">endless hiking</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
-
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-socks fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">amazing comfort</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
+        {services.map((service) => {
+          const { id, icon, serviceTitle, serviceText } = service;
+          return (
+            <article className="service" key={id}>
+              <span className="service-icon">
+                <i className={icon}></i>
+              </span>
+              <div className="service-info">
+                <h4 className="service-title">{serviceTitle}</h4>
+                <p className="service-text">{serviceText}</p>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
--- src/data/links.js
+++ src/data/links.js
@@ -10,3 +10,27 @@ export const socialLinks = [
   { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
   { id: 3, href: "https://www.twitter.com", icon: "fab fa-squarespace" },
 ];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    serviceTitle: "saving money",
+    serviceText:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperioresofficia. Test - 1",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    serviceTitle: "endless hiking",
+    serviceText:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperioresofficia. Test - 2",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    serviceTitle: "amazing comfort",
+    serviceText:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperioresofficia. Test - 3",
+  },
+];
