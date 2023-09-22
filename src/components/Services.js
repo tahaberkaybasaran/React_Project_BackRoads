3623ab16d15d70202e41135384b6bc8bc51056f0
Trigonometri1 <tahaberkaybasaran@gmail.com>
2023-09-22 18:27:54 +0300

    more component on Services

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Title from "./Title";
 import { services } from "../data/links";
+import Service from "./Service";
 
 const Services = () => {
   return (
@@ -9,18 +10,7 @@ const Services = () => {
 
       <div className="section-center services-center">
         {services.map((service) => {
-          const { id, icon, serviceTitle, serviceText } = service;
-          return (
-            <article className="service" key={id}>
-              <span className="service-icon">
-                <i className={icon}></i>
-              </span>
-              <div className="service-info">
-                <h4 className="service-title">{serviceTitle}</h4>
-                <p className="service-text">{serviceText}</p>
-              </div>
-            </article>
-          );
+          return <Service key={service.id} {...service} />;
         })}
       </div>
     </section>
